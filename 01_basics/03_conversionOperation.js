4de6b39f689cb4984582e25ff1e71f48292734c7
Abhaygupta1101 <144575642+Abhaygupta1101@users.noreply.github.com>
2026-01-03 17:34:00 +0000

    ConversionOperations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+////let score ="33abh" // jb yha null ho ga to value 0 ho gi
+                   // jb yha boolean values true to 1 false 0
+                   //String mai NaN
+
+//// console.log(typeof score);// value = String
+//// console.log(typeof (score));// value in String
+
+//// let valueInNumber = Number(score) //  convert into Number 
+//// console.log(typeof valueInNumber);// value in Number 
+//// console.log(valueInNumber)// Value NaN(Not a number)
+
+
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1
+//false => 0
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+let someNumber =33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
