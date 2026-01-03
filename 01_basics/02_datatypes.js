18985d72a069be98e0d84d17688f817bd4072ad2
Abhaygupta1101 <144575642+Abhaygupta1101@users.noreply.github.com>
2026-01-03 13:53:26 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all JS code as newer version
+//alert(3+3) // we are using nodejs, not browser 
+console.log(3+3)
+console.log("Aman")
+let name = "Kumar"
+let age = 19
+let isLoggedIn = true
+let state = null; 
+/*
+number => 2 to power 53
+bigint => 
+    String => ""
+boolean => true/false
+null => Standalone value // null ka return type Object
+undefined => Return type = undefind
+Symbol =>unique
+
+*/
+//Object // return type
+console.log(typeof "")
