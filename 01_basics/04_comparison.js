5b1fc275b74aa156cc1a508fc613df6702ab89f7
Abhaygupta1101 <144575642+Abhaygupta1101@users.noreply.github.com>
2026-01-03 18:52:58 +0000

    comparison

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -5,3 +5,12 @@
 console.log(null > 0)
 console.log(null == 0)
 console.log(null>=0)
+
+
+console.log(undefined == 0)
+console.log(undefined > 0)
+console.log(undefined< 0)
+// ===  value ko strictele check karta hai
+console.log("2" === 2)
+// console.log(null == 0)
+// console.log(null>=0)
