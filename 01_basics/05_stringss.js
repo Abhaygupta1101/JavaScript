b558fde6da6f33b2c106efe4069f8a95c430a3fb
Abhaygupta1101 <144575642+Abhaygupta1101@users.noreply.github.com>
2026-01-04 19:45:58 +0000

    update

---
+++ 01_basics/05_stringss.js
@@ -0,0 +1,14 @@
+const name = "Abhay"
+const repoCount =50;
+//console.log( repoCount + "Value");
+console.log(`hello my name is ${name} and my school is {repoCount}`);
+const gameName = new String('Abhay')
+console.log(gameName[0])
+console.log(gameName.__proto__);
+ console.log(gameName.length);
+ console.log(gameName.toUpperCase())
+ console.log(gameName.charAt('b'))
+ const newString = gameName.substring(0,4)
+ console.log(newString);
+  console.log("   newString  ");
+ console.log(newStringOne.trim())
