211b883a3b0d28c902d53ed6d1e11bcb2e99a801
Abhaygupta1101 <144575642+Abhaygupta1101@users.noreply.github.com>
2026-01-05 11:14:52 +0000

    Math&Num

---
+++ 01_basics/06_numOrmaths.js
@@ -0,0 +1,29 @@
+// const score  =400
+// const balance = new Number(100)
+// console.log(balance);
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2))
+
+
+// const hundreds = 100000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+//+++++++++++++++++++++ Maths +++++++++++++
+
+// console.log(Math)
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.celi(4.6));
+// console.log(Math.floor(4.6));
+// console.log(Math.min(4.,8,9,67,6));
+// console.log(Math.max(4.,8,9,67,6));
+
+console.log(Math.random());
+console.log(Math.random()*10+1);
+
+
+const min = 10
+const max = 20 
+console.log(Math.floor(Math.random()*(max -min +1))+ min)
