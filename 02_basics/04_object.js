b31fbcd86e67cedfdc7bf7676155db2962ee85e7
Abhaygupta1101 <144575642+Abhaygupta1101@users.noreply.github.com>
2026-01-07 19:15:27 +0000

    obj

---
+++ 02_basics/04_object.js
@@ -0,0 +1,11 @@
+//const tinderUser = new Object()//this  single turn object
+const  tinderUser ={}//this is non single turn object
+tinderUser.id = "123abc";
+tinderUser.name="abahay"
+tinderUser.isLoggedIn = false;
+//console.log(tinderUser);
+
+
+const regularUser = {
+    email:""
+}
