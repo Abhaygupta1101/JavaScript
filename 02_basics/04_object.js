c916274b04721f927d0a676175e368e7cd52f9da
Abhaygupta1101 <144575642+Abhaygupta1101@users.noreply.github.com>
2026-01-12 17:02:56 +0000

    JSON

--- 02_basics/04_object.js
+++ 02_basics/04_object.js
@@ -5,7 +5,69 @@ tinderUser.name="abahay"
 tinderUser.isLoggedIn = false;
 //console.log(tinderUser);
 
-
+//Object  k ander  object ko  call kar na
 const regularUser = {
-    email:""
+    email:"abhaygupta9302@.com",
+    fullname:{
+        userfullname:{
+           firstname :"abhay",
+           lastname:"Gupta" 
+        }
+    }
 }
+//console.log(regularUser.fullname.userfullname);
+    
+
+//objects k ander object ko call kar na
+
+const obj1 ={1 : "a", 2 : "b"}
+const obj2 ={3 : "a", 4 : "b"}
+//const obj3 ={obj1, obj2}
+//const obj3 = Object.assign({}, obj1 ,obj2)
+const obj3 ={...obj1, ...obj2}
+//console.log(obj3);
+
+const users = [
+    {
+        id : 1,
+        email: " abhaygupta@.com"
+    },
+    {
+        id:2,
+        email: " ashu.com"
+    }
+]
+users[1].email
+//console.log(tinderUser);
+//console.log(Object.keys(tinderUser))
+
+
+
+//Objects destructuring and JSON API
+
+
+const course = {
+    coursename: "English ",
+    price :"999",
+    courseInstructor:"don't share"
+}
+
+//{} yja apne maan se name de sak te hai 
+// destructure kar sak te hai
+const {price}= course
+console.log(price);
+
+
+// ++++++++++++ API in java Script
+//Object
+// {
+//     "name": "abhay",
+// }
+
+
+//Arrays
+// [
+//     {},
+//     {},
+//     {}
+// ]
